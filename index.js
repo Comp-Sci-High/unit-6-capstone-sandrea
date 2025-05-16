770300c9a90ad29270766f948e2866a9d661c3a6
K0RE222 <160282348+K0RE222@users.noreply.github.com>
2025-05-16 14:25:20 +0000

    added to the site

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const { name } = require("ejs");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.path}`);
+    next();
+});
+
+const assignmentSchema = new mongoose.Schema(
+    {
+         name: { type: String, required: true },
+         description: { type: String },
+         grade: { type: Number, default: 0 },
+         completionStatus: { type: Boolean, default: false }
+    }
+);
+
+const Assignment = mongoose.model("Assignment", assignmentSchema, "Assignments");
+
+let assignmentList = [""]
+
+
+app.get("/", async (req, res) => {
+    const assignments = await Assignment.find({});
+    res.render("index.ejs", assignments);
+});
+
+
+app.patch("/user/:name", async (req,res) =>{
+    const res = await Assignment.findOneAndUpdate({name: req.params.name}, {req:body}, {new:true})
+    res.render("assignments.ejs", res)
+
+
+app.delete("/user/:name", async (req,res) =>{
+    const res = await Assignment.findOneAndDelete({name: req.params.name})
+    res.render("assignments.ejs", res)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function startServer() {
+    // Add your SRV string, make sure that the database is called SE12
+    await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.2jxgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+    // Uncomment this and run ONCE to add some students for editing and deleting
+    //prepopulateDb()
+
+    app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+
+startServer();
+
+
+
+
+
+
+
 
---
+++ public/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault()
+
+    const assignmentData = new moreData(form)
+    const reqBody = Object.fromEntries(assignmentData)
+    const response = await fetch("/add", {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(reqBody)
+    })
+    const data = await response.json()
+    console.log(data)
+})
